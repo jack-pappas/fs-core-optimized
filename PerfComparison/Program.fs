9972cc2414524c38d6e76681db4b9389dd3f7cf2
Jack Pappas <jackp@server.fake>
2013-04-14 21:09:45 -0400

    Created the OriginalCore.Tests project, which runs the unit tests against the built-in FSharp.Core library. Created the PerfComparison project where we'll implement performance comparisons between the collections in FSharpCore and the built-in FSharp.Core.

---
+++ PerfComparison/Program.fs
@@ -0,0 +1,14 @@
+﻿// FSharp.Core vs. FSharpCore performance comparisons.
+
+open System.Diagnostics
+
+let randArray count =
+    let rand = System.Random ()
+    Array.init count <| fun _ ->
+        rand.Next ()
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
